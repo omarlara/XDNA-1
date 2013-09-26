eef512622a3624006895f16ccc9e0f5e5db31920
Carlos <carlosepp@gmail.com>
2013-09-26 15:23:05 -0500

    responsive tables fix

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -1,17 +1,15 @@
 (function($)
 {
-var $sptableList = $("table.ms-listviewtable > tbody");
-var $headers = $sptableList.find("> tr.ms-viewheadertr > th");
-var $items = $sptableList.find("> tr.ms-itmhover");
-$items.each(function() {
-$item = $(this);
-var counter = 0;
-$cols = $item.find(" > td");
-$cols.each(function() {
-$mobileHeader = $("<div class='mob-header'></div>");
-$mobileHeader.append($($headers[counter]).html());
-$(this).prepend($mobileHeader);
-counter++;
-});
-});
+
+
+
+
+var head= $('.ms-viewheadertr'),
+    headText = head.html();
+
+    head.parents('table').prepend(headText);
+    head.remove();
+
+
+
 }(jQuery));
