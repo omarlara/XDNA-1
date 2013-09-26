60a1cdc9206938eaedd5efe4e485fb72c429753c
Carlos <carlosepp@gmail.com>
2013-09-26 15:34:13 -0500

    quick bug fix

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -4,9 +4,9 @@ $(document).ready(function(e){
 var head= $('.ms-viewheadertr'),
     headText = head.html();
 
-    head.parents('table').prepend(headText);
+    head.parents('table').prepend('<thead>'+headText+'</thead>');
     head.remove();
-    head.replaceWith($('<thead>' + this.innerHTML + '</thead>'));
+
 });
 
 
