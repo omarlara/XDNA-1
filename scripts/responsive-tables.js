0b6f366f87e810d99eb1be67b177b2359d5ef240
Carlos <carlosepp@gmail.com>
2013-09-26 15:37:32 -0500

    missing ()

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -4,7 +4,7 @@ $(document).ready(function(e){
 var head= $('.ms-viewheadertr'),
     headText = head.html();
 
-    head.parent.parent().('table').prepend('<thead>'+headText+'</thead>');
+    head.parent().parent().('table').prepend('<thead>'+headText+'</thead>');
     head.remove();
 
 });
