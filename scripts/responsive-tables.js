dea4a6cb919eb17a7f30cec7d28d3ae09bc2e7c0
Carlos <carlosepp@gmail.com>
2013-10-02 14:49:15 -0500

    loop deletion

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -10,21 +10,19 @@ var head= $('.ms-viewheadertr'),
   var stackedTable = $('table#onetidMngFieldRptrTable'),
       headings = new Array(),
       headingCount = 0;
-    stackedTable.find('tbody tr th').each(function(e){
-
-      headings[headingCount] = $(this).text();
-
       
-      stackedTable.find('tr').not('first-child').each(function(e){
+      stackedTable.find('tbody tr th').each(function(e){
+      headings[headingCount] = $(this).text();
+      // stackedTable.find('tr').not('first-child').each(function(e){
         
-        var    countIterator = 0;
+        var   countIterator = 0;
         $(this).find('.ms-vb2').each(function(e){
             var fieldValue = $(this).html();
             $(this).html('<span class="field-name">'+ headings[countIterator] + '</span> ' + fieldValue);
             countIterator++;
             console.log('countIterator: ' + countIterator + 'headings[countIterator]: ' + headings[countIterator]);
         });
-      });
+      //});
       headingCount++;
     }).parent().remove(); 
 
