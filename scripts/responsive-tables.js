3b1947ed77ea35c623c0053ba96a28ada1cf09a4
Carlos <carlosepp@gmail.com>
2013-10-02 11:01:35 -0500

    More specific selector in responsive tables.js

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -1,12 +1,19 @@
 
 
 $(document).ready(function(e){
-var head= $('.ms-viewheadertr'),
+var head= $('table tbody tr:first-child'),
     headText = head.html();
     //**Takking the headers out as thead elements
     head.parent().parent().prepend('<thead>'+headText+'</thead>');
     head.remove();
 
+/**var stackedTable = $('#onetidMngCytpeRptrTable');
+    stackedTable.find('th').each(function(e){
+      var textValue = $(this).text();
+      stackedTable.find('tr').each(function(e){
+        $(this).find()
+      });
+    }); **/
 
 });
 
