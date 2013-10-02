6ad53a2cfe883c055e4bd0c2ea8c904163c25657
Carlos <carlosepp@gmail.com>
2013-10-02 11:23:23 -0500

    onetidMngCytpeRptrTable

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -7,13 +7,26 @@ var head= $('.ms-viewheadertr'),
     head.parent().parent().prepend('<thead>'+headText+'</thead>');
     head.remove();
 
-/**var stackedTable = $('#onetidMngCytpeRptrTable');
+  var stackedTable = $('#onetidMngCytpeRptrTable'),
+      headings = new Array(),
+      headingCount = 0;
     stackedTable.find('th').each(function(e){
-      var textValue = $(this).text();
-      stackedTable.find('tr').each(function(e){
-        $(this).find()
+
+      headings[headingCount] = $(this).text();
+      consold.log(headingCount + 'Heading: '+ headings[headingCount]);
+
+      
+      stackedTable.find('tr').not('first-child').each(function(e){
+        
+        var    countIterator = 0;
+        $(this).find('.ms-vb2').each(function(e){
+            var fieldValue = $(this).text();
+            $(this).html(headings[countIterator] + ' ' + fieldValue);
+            countIterator++;
+        });
       });
-    }); **/
+      headingCount++;
+    }); 
 
 });
 
