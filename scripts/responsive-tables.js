1fb4c102e4589c2104d985758b7595c0f0d8f56f
Carlos <carlosepp@gmail.com>
2013-10-04 12:54:27 -0500

    Rolled back

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -1,18 +1,18 @@
 
 
 $(document).ready(function(e){
-/**var head= $('.ms-viewheadertr'),
+var head= $('.ms-viewheadertr'),
     headText = head.html();
     //**Takking the headers out as thead elements
-    head.parent().parent().prepend('<thead>'+headText+'<w/thead>');
-    head.remove();**/
+    head.parent().parent().prepend('<thead>'+headText+'</thead>');
+    head.remove();
 
   var stackedTable = $('table#onetidMngFieldRptrTable, table#onetidMngCytpeRptrTable'),
       headings = new Array(),
       headingCount = 0;
       
       stackedTable.find('tbody tr th').each(function(e){
-    /**  headings[headingCount] = $(this).text();
+      headings[headingCount] = $(this).text();
       // stackedTable.find('tr').not('first-child').each(function(e){
         
         var   countIterator = 0;
@@ -23,22 +23,11 @@ $(document).ready(function(e){
             console.log('countIterator: ' + countIterator + 'headings[countIterator]: ' + headings[countIterator]);
         });
       //});
-      headingCount++; **/
+      headingCount++;
     }).parent().remove();
 
-     var $sptableList = $("table.ms-listviewtable > tbody");
-      var $headers = $sptableList.find("> tr.ms-viewheadertr > th");
-      var $items = $sptableList.find("> tr.ms-itmhover");
-      $items.each(function() {
-      $item = $(this);
-      var counter = 0;
-      $cols = $item.find(" > td");
-      $cols.each(function() {
-      $mobileHeader = $("<div class='mob-header'></div>");
-      $mobileHeader.append($($headers[counter]).html());
-      $(this).prepend($mobileHeader);
-      counter++;
-      });
+
+     
 
 });
 
