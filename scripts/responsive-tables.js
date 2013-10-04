5fbce2dc17825f6e3c94bc85da3395dc7eea7489
Carlos <carlosepp@gmail.com>
2013-10-04 17:09:24 -0500

    debug

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -30,19 +30,6 @@ $(document).ready(function(e){
 **/
 
 
-  var $sptableList = $("table.ms-listviewtable > tbody");
-  var $headers = $sptableList.find("> tr.ms-viewheadertr > th");
-  var $items = $sptableList.find("> tr.ms-itmhover");
-  $items.each(function() {
-    $item = $(this);
-    var counter = 0;
-    $cols = $item.find(" > td");
-    $cols.each(function() {
-    $mobileHeader = $("<div class='mob-header'></div>");
-    $mobileHeader.append($($headers[counter]).html());
-    $(this).prepend($mobileHeader);
-    counter++;
-  });
 });
 
 
