53d66c086ca195b4bb5d0e937689fcb944e14c28
Carlos <carlosepp@gmail.com>
2013-10-04 12:01:35 -0500

    new table approach

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -1,18 +1,18 @@
 
 
 $(document).ready(function(e){
-var head= $('.ms-viewheadertr'),
+/**var head= $('.ms-viewheadertr'),
     headText = head.html();
     //**Takking the headers out as thead elements
     head.parent().parent().prepend('<thead>'+headText+'</thead>');
-    head.remove();
+    head.remove();**/
 
   var stackedTable = $('table#onetidMngFieldRptrTable, table#onetidMngCytpeRptrTable'),
       headings = new Array(),
       headingCount = 0;
       
       stackedTable.find('tbody tr th').each(function(e){
-      headings[headingCount] = $(this).text();
+    /**  headings[headingCount] = $(this).text();
       // stackedTable.find('tr').not('first-child').each(function(e){
         
         var   countIterator = 0;
@@ -23,8 +23,27 @@ var head= $('.ms-viewheadertr'),
             console.log('countIterator: ' + countIterator + 'headings[countIterator]: ' + headings[countIterator]);
         });
       //});
-      headingCount++;
-    }).parent().remove(); 
+      headingCount++; **/
+    }).parent().remove();
+
+
+    (function($)
+    {
+      var $sptableList = $("table.ms-listviewtable > tbody");
+      var $headers = $sptableList.find("> tr.ms-viewheadertr > th");
+      var $items = $sptableList.find("> tr.ms-itmhover");
+      $items.each(function() {
+      $item = $(this);
+      var counter = 0;
+      $cols = $item.find(" > td");
+      $cols.each(function() {
+      $mobileHeader = $("<div class='mob-header'></div>");
+      $mobileHeader.append($($headers[counter]).html());
+      $(this).prepend($mobileHeader);
+      counter++;
+      });
+    });
+    }(jQuery)); 
 
 });
 
