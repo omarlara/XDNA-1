3b53172ea4dfd7f1c984276fcc1ec63c2cdbfd72
Carlos <carlosepp@gmail.com>
2013-10-09 17:38:02 -0500

    456

--- scripts/responsive-tables.js
+++ scripts/responsive-tables.js
@@ -82,7 +82,7 @@ var table = $('#main-content table'),
           .on({
             click: function(){
 
-              $('.accordion-title').not(this).slideUp(100);
+              $('.accordion-title').not(this).next().slideUp(100);
               $(this).toggleClass("active").next().slideToggle(200);
               
             }
